6738d73e978a5beeaa4175fb8ae0c566acf427d1
Simon Qi <smilexx2@gmail.com>
2017-07-01 22:40:28 +1000

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'))
+});
+
+//Watch task
+gulp.task('default', ['styles'], function() {
+    gulp.watch('scss/**/*.scss', ['styles']);
+});
---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(".portfolio-item img").click(function(e) {
+    e.preventDefault();
+    $("#overlay").fadeIn(200);
+    let $image = $('<img>').attr('src', $(this).attr('src'));
+    $('.image-container').append($image);
+});
+
+$("#overlay").click(function() {
+    $(this).fadeOut(200, function() {
+        $('.image-container').empty();
+    });
+}).find(".image-container").click(function() {
+    return false;
+});
+
+$(".menu-toggle").click(function() {
+    $(this).toggleClass("on");
+    $(".main-nav").slideToggle();
+    return false;
+});
